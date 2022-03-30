5cb1ee0250556af1ac1bba77c8cfc44db2763541
kouyoumart <armand.kouyoumdjian@artefact.com>
2022-03-30 15:06:14 +0200

    analyses & seeds

---
+++ analyses/jinja_test.sql
@@ -0,0 +1,21 @@
+{% set cool_string = 'wow' %}
+{% set cool_number = 3.14 %}
+
+{{ cool_string }}, Pi, {{cool_number}} is cool
+
+{% set my_animals = ['dog', 'cat', 'panther', 'rabbit'] %}
+
+My favorite animal is {{my_animals[0]}}
+
+{% for animal in my_animals%}
+    My favorite animal is the {{animal}}
+{% endfor %}
+
+
+{% set temperature = 65 %}
+
+{% if temperature < 45 %}
+    Time for a cappucino.
+{% else %}
+    Time for a beer. 
+{% endif %}
